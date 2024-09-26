81181a887b6513c33f3b3210816b3fc990639048
Joel Ramos <joelramos@ceroca.cat>
2024-09-26 17:01:43 +0200

    medidaPantalla

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -2,15 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NauJugador : MonoBehaviour
+public class NaveJugador : MonoBehaviour
 {
     private float _vel;
 
-        
-   // Start is called before the first frame update
+    private Vector2 minPantalla, maxPantalla;
+
+    // Start is called before the first frame update
     void Start()
     {
         _vel = 8;
+   
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+        float mMitatIMGX = GetComponent<SpriteRenderer>().bounds.size.x  / 2;
+        float mMitatIMGY = GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        //minPantalla.x = minPantalla.x + 0.75f;
+        //minPantalla.x += 0.75f;
+        minPantalla.x += mMitatIMGX;
+        maxPantalla.x -= mMitatIMGX;
+        minPantalla.y += mMitatIMGY;
+        maxPantalla.y -= mMitatIMGY;
 
     }
 
@@ -19,13 +31,18 @@ public class NauJugador : MonoBehaviour
     {
         float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
         float direccioIndicadaY = Input.GetAxisRaw("Vertical");
-        //debug.Log("X: "+direccioIndicadaX+" - Y: "+direccioIndicadaY);
-        Vector2 direccioIndicada = new Vector2(direccioIndicadaX,direccioIndicadaY).normalized;
+        //Debug.Log("X: "+direccioIndicadaX +" - Y "+ direccioIndicadaY);
+
+        Vector2 direccioindicada = new Vector2(direccioIndicadaX, direccioIndicadaY);
 
         Vector2 novaPos = transform.position;
+        novaPos = novaPos + direccioindicada * _vel * Time.deltaTime;
 
-        novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
 
         transform.position = novaPos;
+   
+   
     }
 }
